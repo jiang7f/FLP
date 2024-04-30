9b74341809973aac1afe94686646895753abd410
jqf <jiang7ff@163.com>
2024-04-30 13:50:08 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='quBLP',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies required by your package
+    ],
+    # classifiers=[
+    #     'Programming Language :: Python :: 3',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent',
+    # ],
+)
