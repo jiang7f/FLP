5a9dbbba22f82076107bb31856fec6d1c4121d0b
jqf <jiang7ff@163.com>
2024-07-12 16:34:28 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,38 +1,23 @@
-# from setuptools import setup, find_packages
-# import os
-
-# def _process_requirements():
-#     packages = open('requirements.txt').read().strip().split('\n')
-#     requires = []
-#     for pkg in packages:
-#         if pkg.startswith('git+ssh'):
-#             return_code = os.system('pip install {}'.format(pkg))
-#             assert return_code == 0, 'error, status_code is: {}, exit!'.format(return_code)
-#         else:
-#             requires.append(pkg)
-#     return requires
-
-# setup(
-#     name='quBLP',
-#     version='1.0.0',
-#     packages=find_packages(),
-#     install_requires=_process_requirements(),
-#     # classifiers=[
-#     #     'Programming Language :: Python :: 3',
-#     #     'License :: OSI Approved :: MIT License',
-#     #     'Operating System :: OS Independent',
-#     # ],
-# )
-
 from setuptools import setup, find_packages
+import os
+
+def _process_requirements():
+    packages = open('requirements.txt').read().strip().split('\n')
+    requires = []
+    for pkg in packages:
+        if pkg.startswith('git+ssh'):
+            return_code = os.system('pip install {}'.format(pkg))
+            assert return_code == 0, 'error, status_code is: {}, exit!'.format(return_code)
+        else:
+            requires.append(pkg)
+    return requires
 
 setup(
     name='quBLP',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=[
-        # Add any dependencies required by your package
-    ],
+    install_requires=[],
+    # install_requires=_process_requirements(),
     # classifiers=[
     #     'Programming Language :: Python :: 3',
     #     'License :: OSI Approved :: MIT License',
